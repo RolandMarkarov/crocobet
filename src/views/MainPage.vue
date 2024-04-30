ea4ea55affe2d69513b9df11480febaf06ddd471
Roland <markarovr@gmail.com>
2024-04-30 04:56:40 +0400

    cleared code and added logo to sidebar

---
+++ src/views/MainPage.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="wrapper">
+    <sidebar/>
+  </div>
+</template>
+
+<script>
+import Sidebar from './Sidebar.vue'
+
+export default {
+  components: {Sidebar},
+  setup() {
+
+
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.wrapper {
+  background: #DFE5E7;
+
+}
+
+</style>
---
+++ src/views/Sidebar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class='sidebar-wrapper'>
+    <HeaderLogo/>
+  </div>
+</template>
+
+
+<script>
+import HeaderLogo from "@/components/HeaderLogo.vue"
+
+export default {
+  components: {
+    HeaderLogo
+  }
+}
+</script>
+<style scoped>
+.sidebar-wrapper {
+  width: 321px;
+  height: 100vh;
+  border-radius:0px 24px 24px 0px ;
+  background-color: #fff;
+}
+</style>
